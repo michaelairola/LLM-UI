9282ab77821c0b0e92f422ec06284be7cfd43d56
michaelairola <me@michaelairola.com>
2025-07-03 15:44:23 -0700

    format of show page is good now

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,16 +1,15 @@
 class ChatsController < ApplicationController
+  include ChatsHelper
   def new
     @chat = Chat.new
+    @chats = Chat.where(user: Current.user)
+    @examples = get_chat_examples()
     render :show
   end
   
   def show
-    Chat.find(params[:id])
-  end
-  
-  def index
-    @chats = Chat.where(user: Current.user)
-    render json: @chats
+    @chat = Chat.find(params[:id])
+    @chats = Chat.where(user: Current.user).where.not(id: @chat.id)
   end
 
   def create
--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,2 +1,9 @@
 module ChatsHelper
+    def get_chat_examples
+        return [
+            "What is your name?",
+            "What is your quest?",
+            "What is the air-speed velocity of an unladen swallow?"
+        ]
+    end
 end
